ea64bfab2d6802781d786c84ac992fd5ccf7eae3
miklavcj <jaka.miklavc@gmail.com>
2021-04-14 23:23:51 +0200

    testing Icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
       ? '/kocke-app/'
-      : '/'
+      : '/',
+      pwa: {
+  name: "Kocke",
+  short_name: "Kocke",
+  theme_color: "#4DBA87",
+  "icons": [
+      {
+          "src": "./img/icons/kocke.png",
+          "sizes": "192x192",
+          "type": "image/png"
+      },
+  ],
+  start_url: ".",
+  display: "standalone",
+  background_color: "#000000"
+}
+    
+      
   }
