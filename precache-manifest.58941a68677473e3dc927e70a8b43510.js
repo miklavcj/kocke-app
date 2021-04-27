a45cc6dfd2383b8aa033eb7a8243c1e649f2997a
miklavcj <jaka.miklavc@gmail.com>
2021-04-27 09:22:31 +0200

    deploy

---
+++ precache-manifest.58941a68677473e3dc927e70a8b43510.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a1d0cfae82e31ef122",
+    "url": "/kocke-app/css/app.52e52cbd.css"
+  },
+  {
+    "revision": "63da52a2ee8ba8038532",
+    "url": "/kocke-app/css/chunk-vendors.af3221a8.css"
+  },
+  {
+    "revision": "6d11cc1a93ea5d5c4043de5a5b42b5e4",
+    "url": "/kocke-app/img/kocke-logo-final.png"
+  },
+  {
+    "revision": "e5e05da8d2422a2e978b4aad7b15ba18",
+    "url": "/kocke-app/img/kocke-svg.svg"
+  },
+  {
+    "revision": "5e6e52775437cf7f4b8728ebdf8c2df0",
+    "url": "/kocke-app/index.html"
+  },
+  {
+    "revision": "57a1d0cfae82e31ef122",
+    "url": "/kocke-app/js/app.d2701023.js"
+  },
+  {
+    "revision": "63da52a2ee8ba8038532",
+    "url": "/kocke-app/js/chunk-vendors.0e03dda3.js"
+  },
+  {
+    "revision": "01d827288087ac17790b82ced5ca334a",
+    "url": "/kocke-app/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/kocke-app/robots.txt"
+  }
+]);
